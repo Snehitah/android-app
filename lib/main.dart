54572a9538279d832818c208c602d69d07cb0396
Radhika Bhoyar <bhoyarradhika17@gmail.com>
2024-04-04 20:58:32 +0530

    Gallery

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 
 void main() {
@@ -33,12 +35,15 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   XFile? _imageFile;
+  String? _predictionResult;
 
   Future<void> _selectImageFromCamera(BuildContext context) async {
     final pickedFile =
         await ImagePicker().pickImage(source: ImageSource.camera);
     setState(() {
       _imageFile = pickedFile;
+      _predictionResult =
+          null; // Reset prediction result when a new image is selected
     });
   }
 
@@ -47,7 +52,36 @@ class _MyHomePageState extends State<MyHomePage> {
         await ImagePicker().pickImage(source: ImageSource.gallery);
     setState(() {
       _imageFile = pickedFile;
+      _predictionResult =
+          null; // Reset prediction result when a new image is selected
     });
+  }
+
+  Future<void> _predictImage() async {
+    if (_imageFile == null) {
+      // Handle case where no image is selected
+      return;
+    }
+
+    var request = http.MultipartRequest(
+        'POST', Uri.parse('http://127.0.0.1:5000/predict'));
+    request.files
+        .add(await http.MultipartFile.fromPath('image', _imageFile!.path));
+
+    try {
+      var response = await request.send();
+      if (response.statusCode == 200) {
+        var jsonResponse = await response.stream.bytesToString();
+        var predictionResult = json.decode(jsonResponse)['result'];
+        setState(() {
+          _predictionResult = predictionResult;
+        });
+      } else {
+        print('Failed to make prediction. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error making prediction: $e');
+    }
   }
 
   @override
@@ -74,6 +108,16 @@ class _MyHomePageState extends State<MyHomePage> {
               SizedBox(height: 20),
               if (_imageFile != null) // Display the picked image if available
                 Image.file(File(_imageFile!.path)),
+              SizedBox(height: 20),
+              if (_predictionResult !=
+                  null) // Show prediction result if available
+                Text('Prediction Result: $_predictionResult'),
+              SizedBox(height: 20),
+              if (_imageFile != null && _predictionResult == null)
+                ElevatedButton(
+                  onPressed: _predictImage,
+                  child: Text('Predict'),
+                ),
             ],
           ),
         ),
